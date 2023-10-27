ce842ffa18dd1c2e03765fe2af49d2b4b45a1799
Evan Roussin <evan.roussin@gmail.com>
2023-10-27 07:35:11 +0200

    Added dependancy to oh-vue-icons & imported main component in main.js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/style.css'
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+// import { icons } from "oh-vue-icons/icons";
 
-createApp(App).use(router).mount('#app')
+addIcons();
+
+createApp(App)
+    .component("v-icon", OhVueIcon)
+    .use(router)
+    .mount('#app')
