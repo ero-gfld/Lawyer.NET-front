423a12a2277dacf3744644190a3c8dde209f86fb
Evan Roussin <evan.roussin@gmail.com>
2023-10-28 17:24:52 +0200

    Added intl with vue-i18n

--- src/main.js
+++ src/main.js
@@ -3,11 +3,24 @@ import App from './App.vue'
 import router from './router'
 import './assets/style.css'
 import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { createI18n } from 'vue-i18n';
 // import { icons } from "oh-vue-icons/icons";
 
 addIcons();
 
+const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: {
+      en: require('./locales/en.json'),
+      fr: require('./locales/fr.json'),
+      it: require('./locales/it.json'),
+      de: require('./locales/de.json'),
+    }
+});
+
 createApp(App)
     .component("v-icon", OhVueIcon)
     .use(router)
+    .use(i18n  )
     .mount('#app')
