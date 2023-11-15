e502c76a10d705b554136ce1e91f573177d0ebe5
lktaouri <mohammedlaktaoui@outlook.it>
2023-11-15 18:03:52 +0100

    added validation

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -5,21 +5,21 @@
       <form @submit.prevent="onRegister" class="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
         <div class="mb-4">
           <label class="block mb-2 text-sm font-bold text-gray-700" for="email">Email:</label>
-          <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="email" id="email" v-model="email" placeholder="Enter email">
+          <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="email" id="email" v-model="email" placeholder="Enter email" required>
         </div>
         <div class="mb-4">
           <label class="block mb-2 text-sm font-bold text-gray-700" for="password">Password:</label>
-          <input class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="password" id="password" v-model="password" placeholder="Enter password">
+          <input class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="password" id="password" v-model="password" placeholder="Enter password" required>
         </div>
         <div class="mb-4">
           <label class="block mb-2 text-sm font-bold text-gray-700" for="name">Name:</label>
-          <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="text" id="name" v-model="name" >
+          <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="text" id="name" v-model="name" required>
         </div>
 
         <!-- Last Name Field -->
         <div class="mb-6">
           <label class="block mb-2 text-sm font-bold text-gray-700" for="lastName">Last Name:</label>
-          <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="text" id="lastName" v-model="lastName" >
+          <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="text" id="lastName" v-model="lastName" required>
         </div>
         <div class="flex items-center justify-between">
           <button class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline" type="submit">Register</button>
