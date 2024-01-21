9c2d8c3422acf11891bbcebf316d81991d5a59af
eroussin <evan.roussin@gmail.com>
2024-01-21 18:13:35 +0100

    Added page selection component

--- src/components/atoms/VButton.vue
+++ src/components/atoms/VButton.vue
@@ -17,11 +17,11 @@ const props = defineProps({
 function getClass() {
   switch (props.buttonType) {
     case ButtonTypes.PRIMARY:
-      return "bg-primary text-white px-3 py-1 rounded-lg text-sm hover:bg-primary-light active:bg-primary-lighter";
+      return "bg-primary text-white px-3 py-1 rounded-lg text-sm hover:bg-primary-light disabled:bg-primary-lighter";
     case ButtonTypes.SECONDARY:
       return "border text-stone-700 font-semibold px-3 py-1 rounded-lg text-sm";
     case ButtonTypes.ICON:
-      return "text-primary hover:text-primary-light active:text-primary-lighter";
+      return "text-primary hover:text-primary-light active:text-primary-lightest disabled:text-primary-lightest";
     case ButtonTypes.NONE:
       return "";
   }
