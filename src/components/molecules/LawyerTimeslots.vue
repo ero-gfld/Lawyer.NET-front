07faa962199fcedb283aa3249ccff0a4ecad82ac
eroussin <evan.roussin@gmail.com>
2024-01-21 20:43:31 +0100

    Added timeslots component + changed how empty timeslots work

---
+++ src/components/molecules/LawyerTimeslots.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import VButton from "@/components/atoms/VButton.vue";
+import VLabel from "@/components/atoms/VLabel.vue";
+import { parse, format } from "date-fns";
+import { defineProps, ref } from "vue";
+
+const props = defineProps<{
+  date: string;
+  timeslots: string[];
+}>();
+
+const timeslotsShown = ref(5);
+
+function getDayOfWeek(date: string) {
+  const parsedDate = parse(date, "yyyy-MM-dd", new Date());
+  return format(parsedDate, "EEEE");
+}
+
+function getFullDay(date: string) {
+  const parsedDate = parse(date, "yyyy-MM-dd", new Date());
+  return format(parsedDate, "dd MMMM yyyy");
+}
+
+function getEmptySlots(timeslots: string[]) {
+  if (timeslots.length < timeslotsShown.value) {
+    return Array(timeslotsShown.value - timeslots.length).fill("--:--");
+  }
+}
+</script>
+
+<template>
+  <div class="flex flex-col place-items-center">
+    <v-label class="text-stone-500 font-semibold">{{
+      getDayOfWeek(props.date)
+    }}</v-label>
+    <v-label class="text-sm">{{ getFullDay(props.date) }}</v-label>
+    <div class="flex flex-col mt-4 gap-y-3">
+      <div
+        v-for="time in timeslots.slice(0, timeslotsShown)"
+        :key="time"
+        class="text-center"
+      >
+        <v-button button-type="timeslot">
+          {{ time }}
+        </v-button>
+      </div>
+      <div v-for="empty in getEmptySlots(timeslots)" :key="empty">
+        <div class="font-semibold text-gray-400 px-4 py-0.5">--:--</div>
+      </div>
+    </div>
+    <div v-if="timeslots.length > timeslotsShown" class="mt-2">
+      <v-label class="text-stone-500">
+        +{{ timeslots.length - timeslotsShown }}
+      </v-label>
+    </div>
+  </div>
+</template>
--- src/components/organisms/LawyerTimetable.vue
+++ src/components/organisms/LawyerTimetable.vue
@@ -1,73 +1,22 @@
 <script setup lang="ts">
-import { parse, format } from "date-fns";
+import LawyerTimeslots from "../molecules/LawyerTimeslots.vue";
 import { ref, defineProps } from "vue";
-import VButton from "@/components/atoms/VButton.vue";
-import VLabel from "@/components/atoms/VLabel.vue";
-
-const EMPTY_TIMESLOT = "--:--";
 
 const props = defineProps<{
   timetable: [string, string[]];
 }>();
 
 const timetable = ref(props.timetable);
-
-function getDayOfWeek(date: string) {
-  const parsedDate = parse(date, "yyyy-MM-dd", new Date());
-  return format(parsedDate, "EEEE");
-}
-
-function getFullDay(date: string) {
-  const parsedDate = parse(date, "yyyy-MM-dd", new Date());
-  return format(parsedDate, "dd MMMM yyyy");
-}
-
-function filledTimetable(): [string, string[]] {
-  const newTimetable = timetable.value;
-  for (const [date, timeslots] of Object.entries(newTimetable)) {
-    console.log(date);
-    if (typeof timeslots === "string") {
-      break;
-    }
-    for (let i = timeslots.length; i < 5; i++) {
-      timeslots.push(EMPTY_TIMESLOT);
-    }
-  }
-  return newTimetable;
-}
 </script>
 
 <template>
   <div class="flex flex-row justify-between">
-    <div
-      v-for="[date, timeslots] in Object.entries(filledTimetable())"
-      :key="date"
-    >
-      <div class="flex flex-col place-items-center">
-        <v-label class="text-stone-500 font-semibold">{{
-          getDayOfWeek(date)
-        }}</v-label>
-        <v-label class="text-sm">{{ getFullDay(date) }}</v-label>
-        <div class="flex flex-col mt-4 gap-y-3">
-          <div
-            v-for="time in timeslots.slice(0, 5)"
-            :key="time"
-            class="text-center"
-          >
-            <v-button button-type="timeslot" v-if="time !== EMPTY_TIMESLOT">
-              {{ time }}
-            </v-button>
-            <div v-else class="font-semibold text-gray-400 px-4 py-0.5">
-              {{ time }}
-            </div>
-          </div>
-        </div>
-        <div v-if="timeslots.length > 5" class="mt-2">
-          <v-label class="text-stone-500">
-            +{{ timeslots.length - 5 }}
-          </v-label>
-        </div>
-      </div>
+    <div v-for="[date, timeslots] in Object.entries(timetable)" :key="date">
+      <lawyer-timeslots
+        v-if="Array.isArray(timeslots)"
+        :date="date"
+        :timeslots="timeslots"
+      />
     </div>
   </div>
 </template>
